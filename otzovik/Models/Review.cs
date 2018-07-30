8e39ae18277a65249a3f746f6b404d740c2a00d3
psychomonkey911 <forboev@gmail.com>
2018-07-30 09:28:03 +0300

    new

---
+++ otzovik/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace otzovik.Models
+{
+    public class Class
+    {
+    }
+}
--- otzovik/Startup.cs
+++ otzovik/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using otzovik.Data;
 using otzovik.Models;
 using otzovik.Services;
@@ -35,7 +36,29 @@ namespace otzovik
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
 
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/login";
+                options.LogoutPath = "/signedout";
+            })
+
+            .AddGoogle(options =>
+            {
+                options.ClientId = "110558674862-i4f383r39e64mgbtub7sme9et6es6had.apps.googleusercontent.com";
+                options.ClientSecret = "BJkAaANq_rUrNWL_PnYTrIUa";
+            })
+
+            .AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"]; //добавить
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"]; //добавить
+            });
+            
             services.AddMvc();
         }
 
